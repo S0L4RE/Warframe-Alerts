0a9c94098bf8f124e47e97dc5633cf5e1d8b2235
rei2hu <thornas.chang@gmail.com>
2017-02-06 12:17:09 -0600

    remove old files

--- classes/BroadcastMessage.js
+++
@@ -1,7 +0,0 @@
-class BroadcastMessage {
-  constructor(type, guid, timeout, interval, func) {
-    this.type = type;
-    this.guid = guid;
-    this.interval = setTimeout(function(){func()}, timeout);
-  }
-}
