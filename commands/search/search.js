51e93dff20f42cc210187030b0c99701589e00cb
rei2hu <thornas.chang@gmail.com>
2017-02-06 22:57:59 -0600

    more argument checking

--- commands/search/search.js
+++ commands/search/search.js
@@ -59,7 +59,7 @@ module.exports = {
   run: (bot, message, args) => {
 		if (args.length === 0) return message.reply(`Loaded files are ${jsons.get("_KEYS")}.`);
     let key = args[0]; // the file name
-    let value = args[1]; // the search term
+    let value = args[1] || ""; // the search term
     let f = jsons.get("_KEYS").filter((e) => {
       return e.toLowerCase().startsWith(key.toLowerCase());
     });
