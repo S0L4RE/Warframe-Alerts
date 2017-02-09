02f948145009cf0575e6926c4ca45d2df84f9899
rei2hu <thornas.chang@gmail.com>
2017-02-08 18:30:43 -0600

    add about command

---
+++ commands/help/about.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "about",
+  descn: "about the bot",
+  run: (bot, message, args, commands) => {
+    let author = message.channel.members.get("84678516477534208").user;
+    message.channel.sendMessage("", {embed:{
+      color: Math.random() * 0xffffff,
+      author: {
+        name: author.username,
+        icon_url: author.avatarURL
+      },
+      title: "Source",
+      url: "https://github.com/rei2hu/wf-bot-2.0",
+      description: "A warframe bot by reimu.",
+      timestamp: new Date()
+    }})
+  }
+}
