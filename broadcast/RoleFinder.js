d1505d7baa97fbb39e420e729bf54805a944a5c5
rei2hu <thornas.chang@gmail.com>
2017-04-10 19:01:19 -0500

    fixed role things

--- broadcast/RoleFinder.js
+++ broadcast/RoleFinder.js
@@ -1,12 +1,13 @@
 module.exports = {
   matchRoles: (event, guild) => {
     const roleNames = event.rewards;
-    const type = event.platform_type === "pc" ? "" : event.platform_type + "_";
+    const type = event.platform_type === "pc" ? "" : event.platform_type;
     let matchingRoles = [];
     for (role of roleNames) {
-      fr = guild.roles.find((r) => (type + role).toLowerCase().includes(r.name.toLowerCase()));
-      if(fr) {
-        matchingRoles.push(fr.toString());
+      // if r startswith and replace i guess
+      fr = guild.roles.filter((r) => r.name.startsWith(type) && (type + role).toLowerCase().includes(r.name.replace(type, "").toLowerCase()));
+      if(fr.size > 0) {
+        fr.forEach((r) => matchingRoles.push(r.toString()));
       }
     }
     return matchingRoles;
