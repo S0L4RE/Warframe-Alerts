bed63b6a37a48c740f7fb0eeab496c07ca0fc601
rei2hu <thornas.chang@gmail.com>
2017-04-15 18:46:07 -0500

    add readable time to worldstates

--- worldstate/Worldstate.js
+++ worldstate/Worldstate.js
@@ -4,6 +4,36 @@ let worldState = {};
 let ps4WorldState = {};
 let xb1WorldState = {};
 
+function enhanceWS(ws) {
+  expireReadableTimes(ws, ws.Time * 1000);
+  return ws;
+}
+
+function expireReadableTimes(obj, wstime) {
+  if (obj["$date"] && obj["$date"]["$numberLong"]) {
+    const difference = (obj["$date"]["$numberLong"] - wstime) / 1000;
+    obj["$date"]["$readable"] = (new Date(parseInt(obj["$date"]["$numberLong"]))).toUTCString();
+    const absDif = Math.abs(difference);
+    if (difference > 0) {
+      obj["$timeTil"] = {
+        $numberLong: absDif,
+        $readable: `${absDif / 3600 << 0}h ${absDif % 3600 / 60 << 0}m ${absDif % 60 << 0}s`
+      }
+    } else {
+      obj["$timeAgo"] = {
+        numberLong: absDif,
+        $readable: `${absDif / 3600 << 0}h ${absDif % 3600 / 60 << 0}m ${absDif % 60 << 0}s`
+      }
+    }
+  } else {
+    for (key of Object.keys(obj)) {
+      if (obj[key] && typeof obj[key] === "object") {
+        expireReadableTimes(obj[key], wstime);
+      }
+    }
+  }
+}
+
 module.exports = {
   getWs: {
     pc: () => worldState,
@@ -16,17 +46,17 @@ module.exports = {
   update: () => {
     superagent.get("http://content.warframe.com/dynamic/worldState.php").end((err, content) => {
       if (err) return console.log(error.status || error.response);
-      worldState = JSON.parse(content.text);
+      worldState = enhanceWS(JSON.parse(content.text));
     })
     // ps4 ws http://content.ps4.warframe.com/dynamic/worldState.php
     superagent.get("http://content.ps4.warframe.com/dynamic/worldState.php").end((err, content) => {
       if (err) return console.log(error.status || error.response);
-      ps4WorldState = JSON.parse(content.text);
+      ps4WorldState = enhanceWS(JSON.parse(content.text));
     })
     // xb1 ws http://content.xb1.warframe.com/dynamic/worldState.php
     superagent.get("http://content.xb1.warframe.com/dynamic/worldState.php").end((err, content) => {
       if (err) return console.log(error.status || error.response);
-      xb1WorldState = JSON.parse(content.text);
+      xb1WorldState = enhanceWS(JSON.parse(content.text));
     })
   }
 }
