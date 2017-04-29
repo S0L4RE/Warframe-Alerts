d2343310705c6d83f8fdfb7358fa74d1b67f2ed4
rei2hu <thornas.chang@gmail.com>
2017-04-28 21:18:20 -0500

    json and fissures command

--- commands/worldstate/darvo.js
+++ commands/worldstate/darvo.js
@@ -25,7 +25,6 @@ module.exports = {
     const timeLeftSeconds = timeLeftTotalSeconds % 60 << 0;
     // let timeLeftSeconds = timeLeftTotalSeconds - (timeLeftHours * 3600) - (timeLeftMinutes * 60);
     let deets = `\`\`\`haskell\n${platform} | Darvo's Daily Deal ${timeLeftHours}h ${timeLeftMinutes}m ${timeLeftSeconds}s remaining\n`; // nice spacing!!
-    // 50-60, 65-80, 80-100 what kind of shitty pattern is this?
     deets += `storeitem:  ${stringThings.padRight(deal.StoreItem, 20)} \n`;
     deets += `original:   ${stringThings.padRight(deal.OriginalPrice, 20)} Plat\n`;
     deets += `discounted: ${stringThings.padRight(deal.SalePrice, 20)} Plat ()${deal.Discount}% off)\n`;
